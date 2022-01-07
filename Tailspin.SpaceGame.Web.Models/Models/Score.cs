686b2a25984181f4484a496d033b73e4b4dad809
DARIEM MACIAS MORA <335286@dadeschools.net>
2022-01-07 09:16:53 -0500

    Adding new property

--- Tailspin.SpaceGame.Web.Models/Models/Score.cs
+++ Tailspin.SpaceGame.Web.Models/Models/Score.cs
@@ -19,5 +19,8 @@ namespace TailSpin.SpaceGame.Web.Models
         // The game region (map) the score is associated with.
         [JsonPropertyName("gameRegion")]
         public string GameRegion { get; set; }
+
+        [JsonPropertyName("gameStype")]
+        public string GameStyle { get; set; }
     }
 }
